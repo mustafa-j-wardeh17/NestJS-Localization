5204b8d2c43ada6aefad4f8019f5202c28e732fb
mustafawardeh <mostafa.wardeh2000@gmail.com>
2024-11-27 11:05:36 +0200

    Create I18n Middleware to reduce the code width

---
+++ src/util/i18nMiddleware.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+import { I18nContext, I18nService } from "nestjs-i18n";
+
+@Injectable()
+export class CustomI18n {
+    constructor(private readonly i18n: I18nService) { }
+
+    translate(key, options) {
+        const lang = I18nContext.current().lang
+        return this.i18n.t(key, { lang, ...options })
+    }
+}
